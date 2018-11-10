8cf76e32a37cc878471704814f169b0a13f8b51c
Cooper Miller <coopermllr@gmail.com>
2018-11-10 15:34:15 -0600

    drop down menu setup and input updates

--- src/components/AdminView/AdminView.js
+++ src/components/AdminView/AdminView.js
@@ -3,10 +3,7 @@ import { connect } from 'react-redux';
 import './AdminView.css';
 
 // material-ui imports
-// import classNames from 'classnames';
-// import { withStyles } from '@material-ui/core/styles';
-// import MenuItem from '@material-ui/core/MenuItem';
-// import TextField from '@material-ui/core/TextField';
+import TextField from '@material-ui/core/TextField';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -15,7 +12,10 @@ import TableRow from '@material-ui/core/TableRow';
 import Button from '@material-ui/core/Button';
 // import DeleteIcon from '@material-ui/icons/Delete';
 // import IconButton from '@material-ui/core/IconButton';
-
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 
 const mapStateToProps = reduxState => ({
@@ -79,20 +79,76 @@ class Admin extends Component {
         this.props.dispatch( { type: 'GET_PROJECTS' } )
     }
 
+
+    menuClick = event => {
+        console.log('in menu click', event.target.value)
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleOpen = () => {
+        this.setState({ open: true });
+      };
+
+    handleClose = () => {
+    this.setState({ open: false });
+  };
+
+
     
 
   render() {
     return (
         <div>
                 <form onSubmit={this.onSubmit}>
-                    <input type='text' name="name" placeholder="name" value={this.state.newProject.name} onChange={this.handleChange} />
+                    {/* <input type='text' name="name" placeholder="name" value={this.state.newProject.name} onChange={this.handleChange} />
                     <input type='text' name="description" placeholder="description" value={this.state.newProject.description} onChange={this.handleChange} />
                     <input type='text' name="thumbnail" placeholder="thumbnail" value={this.state.newProject.thumbnail} onChange={this.handleChange} />
                     <input type='text' name="website" placeholder="website" value={this.state.newProject.website} onChange={this.handleChange} />
                     <input type='text' name="github" placeholder="github" value={this.state.newProject.github} onChange={this.handleChange} />
-                    <input type='text' name="date_completed" placeholder="date_completed" value={this.state.newProject.date_completed} onChange={this.handleChange} />
-                    <input type='text' name="tag_id" placeholder="tag_id" value={this.state.newProject.tag_id} onChange={this.handleChange} />
-                    <input type='submit' value='Add New Project' />
+                    <input type='text' name="date_completed" placeholder="date_completed" value={this.state.newProject.date_completed} onChange={this.handleChange} /> */}
+                    {/* <Button type='text' name="tag_id" placeholder="tag_id" 
+                        onChange={this.menuClick}>
+                    Select a Tag
+                    </Button> */}
+                    <FormControl id="formMenu">
+                    <TextField type='text' name="name" placeholder="name" value={this.state.newProject.name} onChange={this.handleChange} />
+                    <TextField type='text' name="name" placeholder="description" value={this.state.newProject.description} onChange={this.handleChange} />
+                    <TextField type='text' name="name" placeholder="thumbnail" value={this.state.newProject.thumbnail} onChange={this.handleChange} />
+                    <TextField type='text' name="name" placeholder="website" value={this.state.newProject.website} onChange={this.handleChange} />
+                    <TextField type='text' name="name" placeholder="github" value={this.state.newProject.github} onChange={this.handleChange} />
+                    <TextField id="date" label="date" type="date" placeholder="date" name="date_completed" 
+                        value={this.state.newProject.date_completed} onChange={this.handleChange} 
+                        InputLabelProps={{
+                            shrink: true,
+                          }}    
+                    />
+                    <Select
+                        open={this.state.open}
+                        onClose={this.handleClose}
+                        onOpen={this.handleOpen}
+                        value={this.state.newProject.tag_id}
+                        onChange={this.handleChange}
+                        inputProps={{
+                            tag_id: '',
+                        }}
+                        name="tag_id"
+                        >
+                           <MenuItem value=''>
+                           <em>None</em>
+                           </MenuItem>
+                            <MenuItem value={1}>React</MenuItem> 
+                            <MenuItem value={2}>jQuery</MenuItem> 
+                            <MenuItem value={3}>Node</MenuItem> 
+                            <MenuItem value={4}>SQL</MenuItem> 
+                            <MenuItem value={5}>Redux</MenuItem> 
+                            <MenuItem value={6}>HTML</MenuItem> 
+                        </Select>
+                    </FormControl>
+                    <Button type='submit' value='Add New Project'>
+                        Add New Project
+                    </Button>
                 </form>
                         <Table>
                             <TableHead>
