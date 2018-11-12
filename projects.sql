03498db3ea8285e878cddcfc3db69855c42cc13b
Cooper Miller <coopermllr@gmail.com>
2018-11-11 22:11:41 -0600

    Readme and projects.sql update

---
+++ projects.sql
@@ -0,0 +1,17 @@
+--- create database and name 'portfolio'
+
+CREATE TABLE "tags" (
+    "id" SERIAL PRIMARY KEY,
+    "name" varchar(255) NOT NULL
+);
+
+CREATE TABLE "projects" (
+    "id" SERIAL PRIMARY KEY,
+    "name" varchar(255) NOT NULL,
+    "description" varchar(2048),
+    "thumbnail" varchar(2048), 
+    "website" varchar(2048),
+    "github" varchar(2048),
+    "date_completed" date,
+    "tag_id" INT REFERENCES "tags"
+);
