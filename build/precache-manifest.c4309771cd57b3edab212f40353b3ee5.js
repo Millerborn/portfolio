61fefba8d2c79c9e07af470d51c266a6ad249d1c
Cooper Miller <coopermllr@gmail.com>
2018-12-13 11:50:37 -0600

    deploying to Heroku

---
+++ build/precache-manifest.c4309771cd57b3edab212f40353b3ee5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb40f4d3594166287deb",
+    "url": "/static/css/main.6518336e.chunk.css"
+  },
+  {
+    "revision": "eb40f4d3594166287deb",
+    "url": "/static/js/main.eb40f4d3.chunk.js"
+  },
+  {
+    "revision": "d3ad01eb4db92166f94a",
+    "url": "/static/js/1.d3ad01eb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "35689ec52a4633abec3fc287779b85ab",
+    "url": "/index.html"
+  }
+];
